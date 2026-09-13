c5f3ceab46851f04ae024280144fd44dc57619d3
Lenovo User <Lenovo User@DESKTOP-2VDT7AQ>
2026-09-12 20:20:43 -0700

    Agrega el service de libros

---
+++ GestionLibros/Services/LibrosService.cs
@@ -0,0 +1,66 @@
+﻿using GestionLibros.DAL;
+using GestionLibros.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Linq.Expressions;
+
+namespace GestionLibros.Services;
+
+public class LibrosService(IDbContextFactory<Contexto> DbFactory)
+{
+    public async Task<bool> Guardar(Libros libro)
+    {
+        if (!await Existe(libro.LibroId))
+        {
+            return await Insertar(libro);
+        }
+        else
+        {
+            return await Modificar(libro);
+        }
+    }
+
+    public async Task<bool> Existe(int libroId)
+    {
+        await using var contexto = await DbFactory.CreateDbContextAsync();
+        return await contexto.Libros.AnyAsync(l => l.LibroId == libroId);
+    }
+
+    private async Task<bool> Insertar(Libros libro)
+    {
+        await using var contexto = await DbFactory.CreateDbContextAsync();
+        contexto.Libros.Add(libro);
+        return await contexto.SaveChangesAsync() > 0;
+    }
+
+    private async Task<bool> Modificar(Libros libro)
+    {
+        await using var contexto = await DbFactory.CreateDbContextAsync();
+        contexto.Update(libro);
+        return await contexto.SaveChangesAsync() > 0;
+    }
+
+    public async Task<Libros?> Buscar(int libroId)
+    {
+        await using var contexto = await DbFactory.CreateDbContextAsync();
+        return await contexto.Libros
+            .FirstOrDefaultAsync(l => l.LibroId == libroId);
+    }
+
+    public async Task<bool> Eliminar(int libroId)
+    {
+        await using var contexto = await DbFactory.CreateDbContextAsync();
+        return await contexto.Libros
+            .AsNoTracking()
+            .Where(l => l.LibroId == libroId)
+            .ExecuteDeleteAsync() > 0;
+    }
+
+    public async Task<List<Libros>> Listar(Expression<Func<Libros, bool>> criterio)
+    {
+        await using var contexto = await DbFactory.CreateDbContextAsync();
+        return await contexto.Libros
+            .Where(criterio)
+            .AsNoTracking()
+            .ToListAsync();
+    }
+}
