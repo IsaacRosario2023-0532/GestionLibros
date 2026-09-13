c1f08fb3c7af2396b34d5c2b62bfc12f5d1c94e7
Lenovo User <Lenovo User@DESKTOP-2VDT7AQ>
2026-09-12 20:09:37 -0700

    Agrega el contexto junto con la tabla libros

---
+++ GestionLibros/DAL/Contexto.cs
@@ -0,0 +1,11 @@
+﻿namespace GestionLibros.DAL;
+
+using GestionLibros.Models;
+using Microsoft.EntityFrameworkCore;
+
+public class Contexto : DbContext
+{
+    public DbSet<Libros> Libros { get; set; }
+
+    public Contexto(DbContextOptions<Contexto> options) : base(options) { }
+}
