098c6a7cc8d399cba4e795c7275ba8557095b91b
Lenovo User <Lenovo User@DESKTOP-2VDT7AQ>
2026-09-12 20:09:25 -0700

    Agega el modelo libros

---
+++ GestionLibros/Models/Libros.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace GestionLibros.Models;
+
+
+public class Libros
+{
+    [Key]
+    public int LibroId { get; set; }
+
+    [Required(ErrorMessage = "Campo Requerido")]
+    public string Titulo { get; set; } = null!;
+
+    [Required(ErrorMessage = "Campo Requerido")]
+    public string Autor { get; set; } = null!;
+
+    [Required(ErrorMessage = "Campo Requerido")]
+    [Range(1, int.MaxValue, ErrorMessage = "Año no valido")]
+    public int AnoPublicacion { get; set; }
+}
